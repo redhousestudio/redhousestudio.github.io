b9e35946707e472aa1d7d565d8b9a5bdd9aa57d7
Faizan Vahevaria <faizanvahevaria@gmail.com>
2019-01-29 21:53:25 +0530

    New Slider in Disco Page

--- discography.md
+++ discography.md
@@ -2,6 +2,15 @@
 layout: discography
 permalink: /discography/
 title: Discography
+slides:
+  - filename: "1.jpg"
+  - filename: "2.jpg"
+  - filename: "3.jpg"
+  - filename: "4.jpg"
+  - filename: "5.jpg"
+  - filename: "6.jpg"
+  - filename: "7.jpg"
+  - filename: "8.jpg"
 
 ---
 
--- js/script.js
+++ js/script.js
@@ -9,4 +9,39 @@ $(document).ready(function() {
     $("#studio-item-caption").html(imgCaption);
     $("#studio-modal").modal("show");
   });
+
+
+  function shuffleElements($elements) {
+    var i, index1, index2, temp_val;
+  
+    var count = $elements.length;
+    var $parent = $elements.parent();
+    var shuffled_array = [];
+  
+  
+    // populate array of indexes
+    for (i = 0; i < count; i++) {
+      shuffled_array.push(i);
+    }
+  
+    // shuffle indexes
+    for (i = 0; i < count; i++) {
+      index1 = (Math.random() * count) | 0;
+      index2 = (Math.random() * count) | 0;
+  
+      temp_val = shuffled_array[index1];
+      shuffled_array[index1] = shuffled_array[index2];
+      shuffled_array[index2] = temp_val;
+    }
+  
+    // apply random order to elements
+    $elements.detach();
+    for (i = 0; i < count; i++) {
+      $parent.append( $elements.eq(shuffled_array[i]) );
+    }
+  }
+
+  shuffleElements( $("#discoSlider .item" ) );
+  $($("#discoSlider .item" )[0]).addClass("active");
+  
 });
