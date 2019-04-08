1ca5cec9bf106f83ed72b186dae4f4584ca197ba
Faizan Vahevaria <faizanvahevaria@gmail.com>
2019-04-09 01:20:45 +0530

    12 photos and heigh 557

--- js/instafeed.js
+++ js/instafeed.js
@@ -2,7 +2,7 @@ var feed = new Instafeed({
   get: 'user',
   userId: '4169714895',
   accessToken: '4169714895.4727e70.a1d858cd642c42eab46cc994d168262d',
-  limit: 9,
+  limit: 12,
   template: '<div class="col-sm-4 col-xs-4 studio-item"><a href="{{link}}" target="_blank"><img src="{{image}}" alt=""></a></div>'
 });
 feed.run();
